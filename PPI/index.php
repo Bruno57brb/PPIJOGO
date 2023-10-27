e032313d80402c3dcd602bf27c52a9dbccb1e2a2
lorenzoMarty <lorenzo.2022310934@aluno.iffar.edu.br>
2023-10-27 10:37:19 -0300

    coloquei index

---
+++ PPI/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>PPI</title>
+</head>
+<body>
+    <label class="titulo">Enigma Capital</label><br>
+    <button type="button" class="iniciar"><a href="jogo.php">INICIAR</a></button>
+    <button type="button" class="crud"><a href="./crud/cadHistoria.php">cadastrarHistoria</a></button>
+    <button type="button" class="crud"><a href="./crud/cadEscolhas.php">cadastrarEscolhas</a></button>
+    <div class="sinopse">Em "(título do jogo)", você é um jovem dos anos 2000 que descobre um segredo sombrio sobre sua família. Determinado a recuperar a honra e a riqueza perdida da família após seu pai revelar um terrível assassinato e roubo em 1973, você se embarca em uma jornada perigosa para descobrir a verdade. Envolto em mistério e tragédia, você se vê em um jogo de gato e rato com um assassino decidido a proteger seu segredo. Cada pista revela mais sobre a história obscura da sua família, enquanto suas escolhas moldam não apenas seu destino, mas também o das gerações futuras. Suas decisões têm consequências profundas e imprevisíveis, levando a um final emocionante onde a verdade será revelada, mas a que custo? O desfecho da história está nas suas mãos, mas lembre-se, nem todas as escolhas levam a finais felizes.</div>
+</body>
+</html>
