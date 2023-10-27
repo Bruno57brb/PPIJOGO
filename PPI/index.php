6541c1353587da8c35e20efaf9b4f41b8a9865ee
lorenzoMarty <lorenzo.2022310934@aluno.iffar.edu.br>
2023-10-27 10:53:00 -0300

    Mexi no cenario

--- PPI/index.php
+++ PPI/index.php
@@ -10,6 +10,7 @@
     <button type="button" class="iniciar"><a href="jogo.php">INICIAR</a></button>
     <button type="button" class="crud"><a href="./crud/cadHistoria.php">cadastrarHistoria</a></button>
     <button type="button" class="crud"><a href="./crud/cadEscolhas.php">cadastrarEscolhas</a></button>
-    <div class="sinopse">Em "(título do jogo)", você é um jovem dos anos 2000 que descobre um segredo sombrio sobre sua família. Determinado a recuperar a honra e a riqueza perdida da família após seu pai revelar um terrível assassinato e roubo em 1973, você se embarca em uma jornada perigosa para descobrir a verdade. Envolto em mistério e tragédia, você se vê em um jogo de gato e rato com um assassino decidido a proteger seu segredo. Cada pista revela mais sobre a história obscura da sua família, enquanto suas escolhas moldam não apenas seu destino, mas também o das gerações futuras. Suas decisões têm consequências profundas e imprevisíveis, levando a um final emocionante onde a verdade será revelada, mas a que custo? O desfecho da história está nas suas mãos, mas lembre-se, nem todas as escolhas levam a finais felizes.</div>
+    <div class="sinopse">Em Enigma Capital, você assume o papel de um jovem dos anos 2000 que, após descobrir um segredo obscuro sobre sua família, se vê imerso em um mundo de mistério e tragédia. Quando seu pai revela uma história terrível de sumiço e perda que assolou sua família em 1972, o jovem decide desvendar os segredos do passado.
+</div>
 </body>
 </html>
