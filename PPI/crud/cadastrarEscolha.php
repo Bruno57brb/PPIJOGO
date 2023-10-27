b92e87ec78f8ced73acb0b0c1bd46e74ebeac884
lorenzoMarty <lorenzo.2022310934@aluno.iffar.edu.br>
2023-10-27 12:20:55 -0300

    s

--- PPI/crud/cadastrarEscolha.php
+++ PPI/crud/cadastrarEscolha.php
@@ -8,7 +8,7 @@ $id = $_POST['id_historia'];
 include("../conecta.php");
 
 // Montar o comando SQL
-$sql = "INSERT INTO escolhas(escolha, id_historia) VALUES ('$escolha', $id)";
+$sql = "INSERT INTO escolhas(escolha, id_historia) VALUES ('$escolha', '$id')";
 // Executar o comando SQL
 if (mysqli_query($conexao, $sql))
     echo "Arquivo enviado com sucesso!";
