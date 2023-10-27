c358b4ea940afaaf23bf18e6c370b9be94af25ce
LorenzoMarty <149172358+LorenzoMarty@users.noreply.github.com>
2023-10-27 20:37:03 -0300

    Add files via upload

--- PPI/jogo.php
+++ PPI/jogo.php
@@ -11,31 +11,46 @@
     $resultadoH = mysqli_query($conexao, $sqlH);
     $sqlE = "SELECT * FROM escolhas WHERE id_escolhas=id_historia";
     $resultadoE = mysqli_query($conexao, $sqlE);
+    $sqlU = "SELECT * FROM usuario";
+    $resultadoU = mysqli_query($conexao, $sqlU);
     ?>
 </head>
 
 <body>
 
     <?php while ($dados1 = mysqli_fetch_assoc($resultadoH)) { ?>
-            <div class="historia">
-                <img src="img/<?php echo $dados1['personagem']; ?>" class="personagem-historia" width="400" height="400">
+        <style>
+                body {
+                    background-image: url('img/<?php echo $dados1['cenario']; ?>');
+                    background-position: center;
+                    background-repeat: no-repeat;
+                    background-attachment: fixed;
+                    background-size: cover;
+                }
+            </style>
+        <div class="historia">
+            <img src="img/<?php echo $dados1['personagem']; ?>" class="personagem-historia" width="400" height="400">
 
-                <div class="balao-historia">
-                    <?php echo $dados1['texto']; ?>
-                </div>
+            <div class="balao-historia">
+                <?php echo $dados1['texto']; ?>
             </div>
-            <?php } ?>
+        </div>
+    <?php } ?>
     <?php while ($dados2 = mysqli_fetch_assoc($resultadoE)) { ?>
-            <div class="escolhas">
-                <div class="balao-escolha">
-                    <p class="texto-escolha"><?php echo $dados2['escolha']; ?></p>
-                </div>
+        <div class="escolhas">
+            <div class="balao-escolha">
+                <p class="texto-escolha">
+                    <?php echo $dados2['escolha']; ?>
+                </p>
             </div>
+        </div>
 
-        <?php } ?>
+    <?php } ?>
+    <?php while ($dados3 = mysqli_fetch_assoc($resultadoU)) { ?>
         <div class="fundo-personagem">
-                <img src="img/personagem-fem.png" class="personagem-escolhas" width="400" height="400">
-                </div>
+            <img src="img/<?php echo $dados3['imagem']; ?>" class="personagem-escolhas" width="400" height="400">
+        </div>
+    <?php } ?>
 </body>
 
 </html>
