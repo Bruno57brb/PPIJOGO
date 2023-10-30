21f5cc7cf0c29cd726239862645b02a930d483ac
lorenzo <lorenzo.@coisa.com>
2023-10-30 03:33:00 -0300

    sla

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <button type="button" class="iniciar"><a href="jogo.php">INICIAR</a></button>
     <button type="button" class="crud"><a href="./crud/cadHistoria.php">cadastrarHistoria</a></button>
     <button type="button" class="crud"><a href="./crud/cadEscolhas.php">cadastrarEscolhas</a></button>
-    <div class="sinopse">Em “título do jogo”, você assume o papel de um jovem dos anos 2000 que, após descobrir um
+    <div class="sinopse">Em Enigma Capital, você assume o papel de um jovem dos anos 2000 que, após descobrir um
         segredo obscuro sobre sua família, se vê imerso em um mundo de mistério e tragédia. Quando seu pai revela uma
         história terrível de sumiço e perda que assolou sua família em 1972, o jovem decide desvendar os segredos do
         passado.
