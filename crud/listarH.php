83fbbbfb35243784c6ed34ba9dfe451b2bad5b1e
lorenzo <lorenzo.@coisa.com>
2023-10-30 03:41:59 -0300

    apagando

---
+++ crud/listarH.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../css/navbar.css">
+  <link rel="stylesheet" href="../css/form.css">
+  <title>PPI</title>
+</head>
+
+<body>
+
+  <style>
+    nav ul .listarH {
+      background: white;
+      border-radius: 5px 5px 5px 5px;
+    }
+  </style>
+  <?php require_once "navbar.php";
+
+  //Conectar ao banco
+  include("../conecta.php");
+
+  //Seleciona todos os dados da tabela lista
+  $sql = "SELECT * FROM historia";
+  //executa o select
+  $resultado = mysqli_query($conexao, $sql);
+
+  //lista os itens
+  echo ' <div class="table">
+  <table>
+    <tr>
+    <strong>
+      <th scope="col">Id</th>
+      <th scope="col">Texto</th>
+      <th scope="col">Opções</th>
+    </strong>
+    </tr>';
+
+  while ($dados = mysqli_fetch_assoc($resultado)) {
+    echo "<td> " . $dados['id_historia'] . " </td>";
+    echo "<td> " . $dados['texto'] . "</td>";
+    echo "<td><a href='../crud/editarH.php?id_historia=" . $dados['id_historia'] . "&texto=" . $dados['texto'] . "'>" . "<img src='../img/settings.png' widht='20' height='20'" . "</a>";
+    echo " <a href='../crud/excluirHistoria.php?id_historia=" . $dados['id_historia'] . "'>" . "<img src='../img/lixeira.png' widht='20' height='20'" . "</a></td>";
+    echo '</tr>';
+  }
+  echo '</table>
+</div>';
+
+  ?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>PPI</title>
+</head>
+
+<body>
+    <label class="titulo"><img src="img/Enigma" width="250px" height="250px"></label><br>
+    <button type="button" class="iniciar"><a href="jogo.php">INICIAR</a></button>
+    <button type="button" class="crud"><a href="./crud/cadHistoria.php">cadastrarHistoria</a></button>
+    <button type="button" class="crud"><a href="./crud/cadEscolhas.php">cadastrarEscolhas</a></button>
+    <div class="sinopse">Em Enigma Capital, você assume o papel de um jovem dos anos 2000 que, após descobrir um
+        segredo obscuro sobre sua família, se vê imerso em um mundo de mistério e tragédia. Quando seu pai revela uma
+        história terrível de sumiço e perda que assolou sua família em 1972, o jovem decide desvendar os segredos do
+        passado.
+
+        Determinado a recuperar a honra de sua família e a riqueza perdida, você parte em uma jornada sombria para
+        descobrir a verdade por trás do desaparecimento de seu parente e o paradeiro dele. No entanto, o caminho para a
+        justiça está cheio de perigos e escolhas difíceis.
+
+        Cada pista descoberta revela uma nova camada de mistério, enquanto você mergulha mais fundo na história obscura
+        de sua família. A linha entre amigo e inimigo se torna turva, e as suas escolhas moldam não apenas o seu
+        destino, mas também o destino das gerações futuras.
+
+    </div>
+</body>
+
+</html>
