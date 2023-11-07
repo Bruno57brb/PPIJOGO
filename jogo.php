9119aab3535a1f2f5fcd5901f5755deed3e8a08b
lorenzo <lorenzo@gmail.com>
2023-11-07 15:40:35 -0300

    array

--- jogo.php
+++ jogo.php
@@ -7,23 +7,18 @@
     <link rel="shortcut icon" href="img/EC.png">
     <title>Enigma Capital</title>
     <?php
-    if ($_GET) {
-        $id = $_GET['id_historia'];
-        $id = explode(" ", $id);
-    } else {
-        $id = 1;
-    }
+    $id = (isset($_GET['id_historia']) ? $_GET['id_historia'] : 0);
     function Historia($id)
     {
         include("conecta.php");
-        $sql = "SELECT * FROM historia WHERE id_historia='$id'";
+        $sql = "SELECT * FROM historia WHERE id_historia=$id";
         $resultado = mysqli_query($conexao, $sql);
         return (mysqli_fetch_assoc($resultado));
     }
     function Escolha($id)
     {
         include("conecta.php");
-        $sql = "SELECT * FROM escolhas WHERE id_historia='$id'";
+        $sql = "SELECT * FROM escolhas WHERE id_historia=$id";
         $resultado = mysqli_query($conexao, $sql);
         return (mysqli_fetch_all($resultado, MYSQLI_ASSOC));
     }
@@ -38,7 +33,6 @@
 </head>
 
 <body>
-
     <?php $dados = Historia($id) ?>
     <style>
         body {
@@ -56,22 +50,22 @@
             <?php echo $dados['texto']; ?>
         </div>
     </div>
-    <?php //var_dump(Escolha($id));die; ?>
     <?php foreach (Escolha($id) as $escolha): ?>
         <div class="escolhas">
             <div class="balao-escolha">
                 <p class="texto-escolha">
                     <?php echo $escolha['escolha'];
                     echo '<a href="jogo.php?id_historia=' . $escolha['id_historia'] . '&id_escolha=' . $escolha['id_escolha'] . '&id_destino=' . $escolha['id_destino'] . '">'; ?>
+                    </a>
                 </p>
             </div>
         </div>
 
     <?php endforeach; ?>
-    <?php //$usuario = Usuario() ?>
-    <!-- <div class="fundo-personagem">
+    <?php $usuario = Usuario() ?>
+     <div class="fundo-personagem">
         <img src="img/<?php echo $usuario['imagem']; ?>" class="personagem-escolhas" width="400" height="400">
-    </div> -->
+    </div>
 </body>
 
 </html>
