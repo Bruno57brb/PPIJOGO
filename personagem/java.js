0885fe2969447ed2d47e1a94296f2160e4f41db1
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2023-11-09 16:52:32 -0300

    java

---
+++ personagem/java.js
@@ -0,0 +1,10 @@
+
+function img01 (){
+    document.getElementById("trocarimg").src="img/pai.png";
+}
+function img02 (){
+    document.getElementById("trocarimg").src="img/personagem-fem.png";
+}
+function img03 (){
+    document.getElementById("trocarimg").src="img/personagem-vilao.png";
+}
