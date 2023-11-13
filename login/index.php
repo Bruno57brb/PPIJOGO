f216fb3c0e9bf36ba8a06c58adb46cbd23ce6e36
LADCSoares <luisa.2021315963@aluno.iffar.edu.br>
2023-11-13 17:13:10 -0300

    login

--- login/index.php
+++ login/index.php
@@ -1,3 +1,44 @@
+<?php
+include('conecta.php');
+
+if(isset($_POST['email']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu e-mail";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+            
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: painel.php");
+
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -30,7 +71,7 @@
        </label></p>
        <p><label class="login__label">
          <span>Senha</span>
-         <input type="password" name="password" class="input">
+         <input type="password" name="senha" class="input">
        </label></p>
      
     
@@ -39,12 +80,12 @@
 
 
       <div class="wrapper">
-        <a href="#"> <button type="button" class="login__button" disabled>
+ <button type="button" class="login__button" disabled>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z" />
             </svg> 
           </button>
-        </a>
+       
        <h1 class="tamanho"> <a href="Restaurar.php" class="login__link">restaurar conta</a>
         <a href="criar.php" class="login__link">criar conta</a></h1>
       </div>
