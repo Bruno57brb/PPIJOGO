84fa937a1b4ce62a23e8696ff9f8204a65cfea77
Bruno57brb <bruno.2022310863@aluno.iffar.edu.br>
2023-11-13 15:06:02 -0300

    usuario

--- personagem/index.php
+++ personagem/index.php
@@ -16,31 +16,62 @@
 
 
     <div class="nome">
-        
-           <h1>Nome:</h1> 
-      
-         
+
+
+        <form>
+            <p><label class="label-nome">
+                    <span>Nome</span>
+                    <input type="text" name="nome" class="input">
+                </label></p>
+        </form>
+
+
+
     </div>
-<div class="fundo"> </div>
+    <div class="fundo"> </div>
     <div class="genero-f">
+        <img src="../img/personagem-fem.png" width="400px" height="400px">
+    </div>
 
-    <img src="../img/personagem-fem.png" width="400px" height="400px">
-            
-</div>
-<div class="icone_fem"> 
-<img src="../img/icone_femenino.png" width="120px" height="120px">
+    <div class="icone_fem">
+        <img src="../img/icone_femenino.png" width="110px" height="110px">
+    </div>
 
-</div>
+    <div class="icone_mas">
+        <img src="../img/icone_masculino.png" width="110px" height="110px">
+    </div>
 
-<div class="icone_mas"> 
-<img src="../img/icone_masculino.png" width="120px" height="120px">
-
-</div>
-
-<div class="borda">  </div>
-<div class="borda_2">  </div>
+    <div class="borda"></div>
 
     <button type="button" class="iniciar"><a href="jogo.php">INICIAR</a></button>
 </body>
+<script>
+const inputs = document.querySelectorAll('.input');
 
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
+}
+
+const handleFocusOut = ({ target }) => {
+  if (target.value === '') {
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');
+  }
+}
+
+const handleChange = () => {
+  const [username, password] = inputs;
+
+  if (username.value && password.value.length >= 8) {
+    button.removeAttribute('disabled');
+  } else {
+    button.setAttribute('disabled', '');
+  }
+}
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
+    </script>
 </html>
