287b77035ed6f7fc0614ae438ea6d6bd17b1b382
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2023-11-22 09:54:09 -0300

    login

--- login/sistema_login/login.php
+++ login/sistema_login/login.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-
 if(empty($_POST) or (empty($_POST['email']) or (empty($_POST['senha'])))) {
-    echo"<script>location.href='';</script>";
+   echo"<script>location.href='';</script>";
 }
 include('conecta.php');
 
@@ -12,16 +11,17 @@ $sql = "SELECT * FROM  usuario
  WHERE email    = '{$usuario}'
  AND  senha = '{$senha}'";
 
- $res = $conexao->query($sql) or die($conexao->error);
- $row = $res->fetch_object();
- $qtd = $res->num_rows;
+$res = $conexao->query($sql) or die($conexao->error);
+$row = $res->fetch_object();
+$qtd = $res->num_rows;
 
- if($qtd > 0 ){
+if($qtd > 0 ){
     $_SESSION['email'] = $usuario;
     $_SESSION['nome'] = $row->nome;
-    header ('Location: ');
+    header ('Location: ../../index.php ');
  }else{
     echo"alert('Usuário e/ou senha incorreto(s)')";
     header ('Location: protect.php');
  }
+
  ?>
--- login/sistema_login/protect.php
+++ login/sistema_login/protect.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION)) {
 }
 
 if(!isset($_SESSION['id_usuario'])) {
-    die("Você não pode acessar esta página porque não está logado.<p><a href=\"..\index.php\">Entrar</a></p>");
+    die("Você não pode acessar esta jogo porque não está logado.<p><a href=\"..\index.php\">Voltar</a></p>");
 }
 
 
