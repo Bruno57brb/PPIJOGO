f5f32bb84be4d9e2da2db96591d87c60c9d8a890
antonio99686 <133412832+antonio99686@users.noreply.github.com>
2023-12-04 12:19:51 -0300

    Update protect.php

--- login/sistema_login/protect.php
+++ login/sistema_login/protect.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION)) {
 }
 
 if(!isset($_SESSION['id_usuario'])) {
-    die("Você não pode acessar esta jogo porque não está logado.<p><a href=\"..\index.php\">Voltar</a></p>");
+    die("Você não pode acessar esta jogo porque não está logado.<p><a href=\"..\..\index.php\">Voltar</a></p>");
 }
 
 
