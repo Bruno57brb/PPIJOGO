24d8ba585e1777e3efbbb9fc871d39a04c5dfa12
lorenzo <lorenzo@gmail.com>
2023-12-04 16:40:32 -0300

    crud

--- login/sistema_login/cadastrar.php
+++ login/sistema_login/cadastrar.php
@@ -2,7 +2,7 @@
 
 include('../../conecta.php');
 
-if(empty($_POST) or (empty($_POST['nome']) or (empty($_POST['email']) or (empty($_POST['senha']))))) {
+if(empty($_POST) or (empty($_POST['email']) or (empty($_POST['senha'])))) {
 $usuario =  $_POST['email'];
 $senha = $_POST['password'];
 $sql = "INSERT INTO usuario (email, senha) VALUES ('$usuario','$senha')";
